d06d777e130bf06f164b5099f297da8dcc874d46
PetrovSergeyAleksandrovich <ps@dsys.ru>
2020-12-25 15:03:13 +0300

    hw done

--- UE4_HW13_4.cpp
+++ UE4_HW13_4.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "helpers.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x, y, result;
+
+    std::cout << "Type first number:";
+    std::cin >> x;
+    std::cout << "Type second number:";
+    std::cin >> y;
+
+    result = squareRoot(x, y);
+
+    std::cout << "\n(x+y)^2 = " << result << std::endl;
 }
 
---
+++ helpers.h
@@ -0,0 +1,4 @@
+int squareRoot(int a, int b)
+{
+	return (a + b) * (a + b);
+}
